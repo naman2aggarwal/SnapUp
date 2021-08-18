a36805837b847f05cd8006047962e1de13dc8a99
Naman Aggarwal <namanaggarwal125@gmail.com>
2021-08-18 21:59:42 +0530

    initial commit

---
+++ src/store.js
@@ -0,0 +1,19 @@
+//Prototype for states to be used
+
+// Products: Product[],
+// SelectedProduct: Product,
+// Cart: Product[],
+// Orders: Product[],
+// Address: {StreetAdress, City, State, ZipCode, Country }
+
+//Description of Product:
+// Product {id, title, price, description, category, quantity, image, color}
+
+//Components
+//App
+// Header
+// ProductContainer
+//     Product
+//     Cart
+//     Orders
+// Footer
